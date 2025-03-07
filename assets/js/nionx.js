d5fad3766f71d371b2c16d54b498267ba7642ca2
rg <rhdhshdfudyehd@gmail.com>
2025-03-07 20:21:57 +0800

    first commit

---
+++ assets/js/nionx.js
@@ -0,0 +1,168 @@
+(function ($) {
+  "use strict";
+  function dynamicCurrentMenuClass(selector) {
+    let FileName = window.location.href.split("/").reverse()[0];
+
+    selector.find("li").each(function () {
+      let anchor = $(this).find("a");
+      if ($(anchor).attr("href") == FileName) {
+        $(this).addClass("current");
+      }
+    });
+    // if any li has .current elmnt add class
+    selector.children("li").each(function () {
+      if ($(this).find(".current").length) {
+        $(this).addClass("current");
+      }
+    });
+    // if no file name return
+    if ("" == FileName) {
+      selector.find("li").eq(0).addClass("current");
+    }
+  }
+
+  if ($(".main-menu__list").length) {
+    // dynamic current class
+    let mainNavUL = $(".main-menu__list");
+    dynamicCurrentMenuClass(mainNavUL);
+  }
+
+  if ($(".main-menu").length && $(".mobile-nav__container").length) {
+    let navContent = document.querySelector(".main-menu").innerHTML;
+    let mobileNavContainer = document.querySelector(".mobile-nav__container");
+    mobileNavContainer.innerHTML = navContent;
+  }
+
+  if ($(".sticky-header").length) {
+    $(".sticky-header")
+      .clone()
+      .insertAfter(".sticky-header")
+      .addClass("sticky-header--cloned");
+  }
+
+  if ($(".mobile-nav__container .main-menu__list").length) {
+    let dropdownAnchor = $(
+      ".mobile-nav__container .main-menu__list .dropdown > a"
+    );
+    dropdownAnchor.each(function () {
+      let self = $(this);
+      let toggleBtn = document.createElement("BUTTON");
+      toggleBtn.setAttribute("aria-label", "dropdown toggler");
+      toggleBtn.innerHTML = "<i class='fa fa-angle-down'></i>";
+      self.append(function () {
+        return toggleBtn;
+      });
+      self.find("button").on("click", function (e) {
+        e.preventDefault();
+        let self = $(this);
+        self.toggleClass("expanded");
+        self.parent().toggleClass("expanded");
+        self.parent().parent().children("ul").slideToggle();
+      });
+    });
+  }
+
+  //Show Popup menu
+  $(document).on("click", ".megamenu-clickable--toggler > a", function (e) {
+    $("body").toggleClass("megamenu-popup-active");
+    $(this).parent().find("ul").toggleClass("megamenu-clickable--active");
+    e.preventDefault();
+  });
+  $(document).on("click", ".megamenu-clickable--close", function (e) {
+    $("body").removeClass("megamenu-popup-active");
+    $(".megamenu-clickable--active").removeClass("megamenu-clickable--active");
+    e.preventDefault();
+  });
+
+  if ($(".mobile-nav__toggler").length) {
+    $(".mobile-nav__toggler").on("click", function (e) {
+      e.preventDefault();
+      $(".mobile-nav__wrapper").toggleClass("expanded");
+      $("body").toggleClass("locked");
+    });
+  }
+
+  //accrodion
+  if ($(".nionx-accrodion").length) {
+    var accrodionGrp = $(".nionx-accrodion");
+    accrodionGrp.each(function () {
+      var accrodionName = $(this).data("grp-name");
+      var Self = $(this);
+      var accordion = Self.find(".accrodion");
+      Self.addClass(accrodionName);
+      Self.find(".accrodion .accrodion-content").hide();
+      Self.find(".accrodion.active").find(".accrodion-content").show();
+      accordion.each(function () {
+        $(this)
+          .find(".accrodion-title")
+          .on("click", function () {
+            if ($(this).parent().hasClass("active") === false) {
+              $(".nionx-accrodion." + accrodionName)
+                .find(".accrodion")
+                .removeClass("active");
+              $(".nionx-accrodion." + accrodionName)
+                .find(".accrodion")
+                .find(".accrodion-content")
+                .slideUp();
+              $(this).parent().addClass("active");
+              $(this).parent().find(".accrodion-content").slideDown();
+            }
+          });
+      });
+    });
+  }
+
+  $(".add").on("click", function () {
+    if ($(this).prev().val() < 999) {
+      $(this)
+        .prev()
+        .val(+$(this).prev().val() + 1);
+    }
+  });
+
+  $(".sub").on("click", function () {
+    if ($(this).next().val() > 0) {
+      if ($(this).next().val() > 0)
+        $(this)
+          .next()
+          .val(+$(this).next().val() - 1);
+    }
+  });
+
+  // window scroll event
+  function stickyMenuUpScroll($targetMenu, $toggleClass) {
+    var lastScrollTop = 0;
+    window.addEventListener(
+      "scroll",
+      function () {
+        var st = window.pageYOffset || document.documentElement.scrollTop;
+        if (st > 500) {
+          if (st > lastScrollTop) {
+            // downscroll code
+            $targetMenu.removeClass($toggleClass);
+            // console.log("down");
+          } else {
+            // upscroll code
+            $targetMenu.addClass($toggleClass);
+            // console.log("up");
+          }
+        } else {
+          $targetMenu.removeClass($toggleClass);
+        }
+        lastScrollTop = st;
+      },
+      false
+    );
+  }
+  stickyMenuUpScroll(
+    $(
+      ".sticky-header--normal, .sticky-header-three--normal, .sticky-header-fore--normal"
+    ),
+    "active"
+  );
+
+  $('._tg_url').click(function (){
+    $(this).attr('href', 'https://t.me/yolanda_ggg');
+    gtag('event', 'conversion', {'send_to': 'AW-11500587481/kFe-CLHHmv8ZENmT9Osq'});
+  });
+})(jQuery);
